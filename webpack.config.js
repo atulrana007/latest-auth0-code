a3d9e3552712ce95ddf6684c9b31429ddd91a328
atulrana007 <atul.rana@zopsmart.com>
2021-12-10 12:56:15 +0530

    updated bundle mudit-test-tenant-5.min.js

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
     main: lModules,
   },
   output: {
-    filename: "mudit-test-tenant-4.min.js",
+    filename: "mudit-test-tenant-5.min.js",
     path: path.resolve(__dirname, "dist"),
   },
   module: {
