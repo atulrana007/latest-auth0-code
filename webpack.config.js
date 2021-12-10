cc45ea3c8e3fa2ed5d8e46d53a66ac75778b1fba
atulrana007 <atul.rana@zopsmart.com>
2021-12-10 19:57:10 +0530

    updated bundle mudit-test-tenant-cc-7

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
     main: lModules,
   },
   output: {
-    filename: "mudit-test-tenant-cc-6.min.js",
+    filename: "mudit-test-tenant-cc-7.min.js",
     path: path.resolve(__dirname, "dist"),
   },
   module: {
